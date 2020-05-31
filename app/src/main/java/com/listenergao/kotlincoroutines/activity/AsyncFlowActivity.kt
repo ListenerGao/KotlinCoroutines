2d0e7a640cb8782fd34cc305cd7ff35fa9de4ff2
gaoyashen <gaoyashen@jd.com>
2020-05-31 18:18:07 +0800

    添加 kotlin coroutines flow 测试

--- app/src/main/java/com/listenergao/kotlincoroutines/activity/AsyncFlowActivity.kt
+++ app/src/main/java/com/listenergao/kotlincoroutines/activity/AsyncFlowActivity.kt
@@ -1,8 +1,16 @@
 package com.listenergao.kotlincoroutines.activity
 
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.listenergao.kotlincoroutines.BaseActivity
 import com.listenergao.kotlincoroutines.databinding.ActivityAsyncFlowBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 /**
  * kotlin coroutines 异步流（Flow）
@@ -19,14 +27,72 @@ class AsyncFlowActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mBinding = ActivityAsyncFlowBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
 
         foo().forEach {
             log(TAG, "value:$it")
         }
 
+        lifecycleScope.launch(Dispatchers.IO) {
+//            foo1().forEach {
+//                log(TAG, "sequence value:$it")
+//            }
+
+//            foo2().forEach {
+//                log(TAG, "suspend value:$it")
+//            }
+        }
+
+        lifecycleScope.launch {
+            //启动并发的协程，以验证主协程并未阻塞
+            launch {
+                for (k in 1..3) {
+                    log(TAG, "I'm not blocked:$k")
+                    delay(500)
+                }
+            }
+
+            log(TAG, "flow collect......")
+            //收集这个流
+            foo3().collect {
+                log(TAG, "flow value:$it")
+            }
+        }
+
 
     }
 
+
     private fun foo(): List<Int> = listOf(1, 2, 3)
+
+    private fun foo1(): Sequence<Int> = sequence { //序列构建器，同步计算，会阻塞主线程
+        Log.d(TAG, "foo1")
+        for (i in 1..3) {
+            //模拟延时
+            Thread.sleep(2000)
+            //产生下一个值
+            yield(i)
+        }
+    }
+
+    private suspend fun foo2(): List<Int> {
+        delay(2000)
+        return listOf(1, 2, 3)
+    }
+
+
+    /**
+     * 使用 flow { ... } 函数体中的代码可以挂起。
+     * 不需要再标有 suspend 修饰修饰符
+     */
+    private fun foo3(): Flow<Int> = flow {
+        for (i in 1..3) {
+            //模拟延迟
+            delay(500)
+            //发送下一个值
+            emit(i)
+        }
+    }
 }
